94c573f1b57ef20d9f053710f9ac3f39a2935958
usmanshoaib241 <usmanshoaib241@gmail.com>
2022-04-11 12:51:12 +0500

    l

--- lib/data/urls.dart
+++ lib/data/urls.dart
@@ -1,5 +1,5 @@
-// const String uBaseURL = 'http://admin.briefify.io/api/';
-const String uBaseURL = 'http://test.briefify.io/api/';
+const String uBaseURL = 'http://admin.briefify.io/api/';
+// const String uBaseURL = 'http://test.briefify.io/api/';
 // const String uBaseURL = 'http://briefify.tritech.host/api/';
 
 const uRegisterUser = uBaseURL + 'register';
